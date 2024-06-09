bd7e2696a3376cf4f87abaff4a35fe2b42a60f25
parfenov_gu <gparfyonov@mail.ru>
2024-06-09 18:03:45 +0400

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBashPractice
+{
+    internal class Class1
+    {
+        public Class1(string name, int age, string position)
+        {
+            Name = name;
+            Age = age;
+            Position = position;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Position { get; private set; }
+        public int Salary { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBashPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Gosha = new Class1("Гоша", 27, "Разработчик");
+
+            Console.WriteLine(Gosha.Name);
+        }
+    }
+}
